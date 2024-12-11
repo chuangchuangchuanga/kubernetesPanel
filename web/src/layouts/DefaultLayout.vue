8c40c890d6d71c6e2fcea37f745ec7bef729968b
chuang <498650825@qq.com>
2024-12-11 18:53:30 +0800

    添加前端页面

---
+++ web/src/layouts/DefaultLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-scrollbar>
+          <el-menu
+              default-active="2">
+            <el-menu-item>
+              <span>Navigator Two</span>
+            </el-menu-item>
+          </el-menu>
+        </el-scrollbar>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+
+<script lang="ts" setup>
+
+</script>
---
+++ web/src/router/index.js
@@ -0,0 +1,28 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+
+const DefaultLayout = () => import('@/layouts/DefaultLayout.vue');
+const DeploymentList = () => import('@/layouts/DeploymentList.vue');
+
+
+const routes = [
+    {
+        path: "/kubernetes",
+        component: DefaultLayout,
+        children: [
+            {
+            path: "deployments/list",
+            name: "DeploymentList",
+            component: DeploymentList,
+            meta: { title: "DeploymentList" },
+        },
+        ]
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+})
+
+export default router;
