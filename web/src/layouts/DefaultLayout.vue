3aa703329712d95f0cebd27e5b4ecb6fa6541069
chuang <498650825@qq.com>
2024-12-12 16:14:14 +0800

    1

--- web/src/layouts/DefaultLayout.vue
+++ web/src/layouts/DefaultLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-container class="layout-container-demo" style="height: 500px">
+  <el-container class="layout-container-demo" >
     <el-aside width="200px">
       <el-scrollbar>
         <el-menu :default-openeds="['1', '3']">
@@ -80,8 +80,8 @@
       <el-main>
         <el-scrollbar>
           <el-table :data="tableData">
-            <el-table-column prop="date" label="Date" width="140" />
-            <el-table-column prop="name" label="Name" width="120" />
+            <el-table-column prop="date" label="Date"  />
+            <el-table-column prop="name" label="Name"  />
             <el-table-column prop="address" label="Address" />
           </el-table>
         </el-scrollbar>
